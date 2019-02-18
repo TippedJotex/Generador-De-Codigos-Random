3233026e93007c0c222ada889e15ea921036b5d1
TippedJotex <carlospuno1997@gmail.com>
2019-02-18 06:09:53 -0400

    version 1

---
+++ GeneradorCodigoRandom/src/generadorcodigorandom/GeneradorCodigoRandom.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package generadorcodigorandom;
+
+import java.util.Random;
+
+/**
+ *
+ * @author carlo
+ */
+public class GeneradorCodigoRandom {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Codigo().setVisible(true);
+    }
+}
